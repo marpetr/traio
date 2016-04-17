9d686f699baff87a9e2df235078ae31d56b3c811
Marijonas <marpetr@gmail.com>
2016-04-17 13:17:57 +0300

    'Blah'

--- frontend/app/scripts/controllers/task.js
+++ frontend/app/scripts/controllers/task.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('traioApp')
-  .controller('TaskCtrl', function ($scope, $routeParams) {
+  .controller('TaskCtrl', function ($scope, $routeParams, $sce) {
     $scope.taskPath = $routeParams.path;
+    $scope.pdfViewerPath =
+      '/pdfjs/web/viewer.html?file=' +
+      encodeURIComponent('/tasks/' + $scope.taskPath + '.pdf') +
+      '#locale=lt&zoom=page-width';
   });
